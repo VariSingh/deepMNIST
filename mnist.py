6cad248752d3f57200d6fa3fbe5a1a8298ae65a5
Varinder Singh <varindersingh271990@gmail.com>
2018-07-26 11:37:10 +0530

    added code for weighted sum of input and biases for all layers

--- mnist.py
+++ mnist.py
@@ -33,8 +33,23 @@ def neural_network():
     'biases':tf.Variable(tf.random_normal([classes]))
     }
 
+    layer1 = tf.add(tf.matmul(data,hidden_layer1['weights']),hidden_layer1['biases'])
+    layer1 = tf.nn.rely(layer1)
 
+    layer2 = tf.add(tf.matmul(layer1,hidden_layer2['weights']),hidden_layer2['biases'])
+    layer2 = tf.nn.rely(layer2)
+
+    layer3 = tf.add(tf.matmul(layer2,hidden_layer3['weights']),hidden_layer3['biases'])
+    layer3 = tf.nn.rely(layer3)
+
+    output = tf.add(tf.matmul(layer3,output_layer['weights']),output_layer['biases'])
+    output = tf.nn.rely(output)
+
+    return output
+
+
+# print(test)
 # init = tf.global_variables_initializer()
 # with tf.Session() as sess:
-#     sess.run(init)
+#     z = sess.run(test)
 #     print(z)
